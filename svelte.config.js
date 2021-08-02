dc3b39cf9461ec8184ad8bfb9e761038d089d43a
Andrew <andrewchang17@hotmail.com>
2021-08-02 16:07:03 -0700

    adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,14 @@
 import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-		adapter: require('@sveltejs/adapter-static')(),
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	},
