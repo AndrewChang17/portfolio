48a604e30008ede0229274718bdc2b5566c766d9
Andrew <andrewchang17@hotmail.com>
2021-08-02 15:57:00 -0700

    tailwind jit

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
+import preprocess from "svelte-preprocess";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
+    kit: {
+		adapter: require('@sveltejs/adapter-static')(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
-	}
+	},
+
+    preprocess: [preprocess({
+        "postcss": true
+    })]
 };
 
 export default config;
